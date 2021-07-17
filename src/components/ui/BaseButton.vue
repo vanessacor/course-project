b562c080c38cea7988a7a757beb7f4235af3274b
vanessa-barros <vanessapcbarros@gmail.com>
2021-07-17 09:43:13 +0100

    feat: create BaseButton

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :class="mode" :type="type">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    mode: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.5rem;
+  font-size: 1em;
+  border: none;
+  border-radius: 2px;
+}
+.default {
+  background-color: transparent;
+}
+.primary {
+  background-color: #f9bc60;
+}
+
+.danger {
+  background-color: #e16162;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import BaseCard from '@/components/ui/BaseCard'
+import BaseButton from '@/components/ui/BaseButton'
 
 const app = createApp(App)
 app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
 
 app.mount("#app");
